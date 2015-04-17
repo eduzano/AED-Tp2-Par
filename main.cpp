b30c2599301703a24b5f655b5c1756ee85ebfb66
eduzano <eduardozano@gmail.com>
2015-04-17 18:03:55 -0300

    Tp 2
    Programa Par/Impar

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	unsigned num;
+	
+	cout<<"Ingrese un numero..\n";
+	cin>>num;
+	if(num%2==0)
+	 cout<<"Es par \n";
+	else
+	 cout<<"Es impar \n";
+	
+}
